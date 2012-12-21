108071a1d36ed66352c7a27b6eb11017ce8daf42
dengjing34 <>
2012-12-21 19:28:59 +0800

    modify class MongoData to optimize query syntax

--- mongo/lib/MongoData.php
+++ mongo/lib/MongoData.php
@@ -9,8 +9,8 @@ class MongoData extends Data{
     const DEFAULT_CONNECT_FLAG = '01';
     const DB_TEST = 'test';//by default select db
     public $_id;
-    private $db, $collection, $className;
-    private static $mongoCollection, $mongoDb;        
+    private $db, $collection;
+    private static $mongoCollection, $mongoDb, $counter = 0;        
 
     final protected function init(array $options) {
         $requiredOptions = array('collection', 'fields');
@@ -18,8 +18,7 @@ class MongoData extends Data{
             throw new Exception("options['{$val}'] is required when init class");
         }
         if (array_values($options['fields']) == $options['fields']) $options['fields'] = array_combine($options['fields'], $options['fields']);        
-        foreach (array('collection', 'fields') as $v) $this->{$v} = $options[$v];
-        $this->className = get_class($this);
+        foreach (array('collection', 'fields') as $v) $this->{$v} = $options[$v];        
         $this->db = isset($options['db']) ? $options['db'] : self::DB_TEST;//if not specify $option['db'] use default
         foreach ($options['fields'] as $field) {
             if (!property_exists($this, $field)) $this->{$field} = null;
@@ -32,6 +31,8 @@ class MongoData extends Data{
             $mongo = new Mongo($config['host'], $config['options']);
             $mongo->setSlaveOkay();
         } catch (MongoConnectionException $e) {
+            throw new Exception($e->getMessage());
+        } catch (Exception $e) {
             throw new Exception($e->getMessage());
         }
         self::$connections[__CLASS__][$flag] = $mongo;        
@@ -66,41 +67,55 @@ class MongoData extends Data{
         if (!is_scalar($id) || !ctype_alnum((string)$id)) throw new Exception("{$id} is not a scalar");
         try {
             $result = $this->getCollection()->findOne(array('_id' => new MongoId($id)));
+            $this->increaseCounter();
         } catch (MongoCursorException $e) {
             throw new Exception($e->getMessage());
         }        
-        if (empty($result)) throw new Exception("{$this->className}::{$id} not found");
-        if (is_null($this->_id)) $this->_id = (string)$result['_id'];
+        if (empty($result)) throw new Exception(get_class($this) . "::{$id} not found");
+        if (is_null($this->_id)) $this->_id = (string)$result['_id'];// can't use $this->parseDoc($result) 
         foreach ($this->fields as $property => $field) $this->{$property} = isset($result[$field]) ? $result[$field] : null;
         return $this;
     }
     
-    public function find(array $query = array(), array $fields = array()) {
-        $result = array();
+    public function find(array $query = array(), array $options = array()) {
+        $result = array();        
         $finalQuery = $this->getQuery($query);//merge self properties and custom query
-        print_r($finalQuery);
+        $allowOptions = array('sort', 'skip', 'limit');        
         try {
-            foreach ($this->getCollection()->find($finalQuery, $fields) as $key => $value) {            
+            $cursor = $this->getCollection()->find($finalQuery);            
+            foreach (array_intersect_key($options, array_flip($allowOptions)) as $method => $val) {                
+                $cursor->{$method}($val);
+            }
+            foreach ($cursor as $key => $value) {            
                 $result[$key] = $this->parseDoc($value);
-            }               
+            }
+            $this->increaseCounter();
         } catch (MongoCursorException $e) {
             throw new Exception($e->getMessage());
         }     
         return $result;
     }
     
+    final private function increaseCounter() {
+        self::$counter++;
+    }
+    
+    public static function getCounter() {
+        return self::$counter;
+    }
+
     private function getQuery(array $query) {
         $result = array();
+        $forbiddenOperators = array('sort', 'skip', 'limit');        
         if (!empty($this->_id)) $result['_id'] = $this->_id;
         foreach ($this->fields as $property => $field) {
             if (is_scalar($this->{$property}) && !empty($this->{$property})) {
-                $result[$field] = $this->{$property}; //标量直接用等于
+                $result[$field] = $this->{$property}; //if is scalar use equal directly
             } elseif (is_array($this->{$property}) && !empty($this->{$property})) {
                 $result[$field] = $this->{$property};
             }
-        }
-        $result += $query;
-        return $result;
+        }        
+        return array_diff_key($result + $query, array_flip($forbiddenOperators));//avoid operators like 'sort', 'skip', 'limit' to be queried
     }    
     
     public function remove($id) {
@@ -116,13 +131,13 @@ class MongoData extends Data{
         $ids = array_filter($ids, function($v){return is_scalar($v) && ctype_alnum((string)$v) ? true : false;});
         if (!empty($ids)) {
             $mongoIds = array_map(function($v) {
-                        return new MongoId($v);
+                return new MongoId($v);
             }, $ids);
-            $cursor = $this->getCollection()->find(array('_id' => array('$in' => $mongoIds)));
-            //$data = iterator_to_array($cursor);                     
+            $cursor = $this->getCollection()->find(array('_id' => array('$in' => $mongoIds)));//don't use $this->find() here, only '_id' will be used to query
+            //$data = iterator_to_array($cursor);
             foreach ($cursor as $key => $value) {
                 $result[$key] = $this->parseDoc($value);
-            }        
+            }
         }
         return $result;
     }
--- mongo/test.php
+++ mongo/test.php
@@ -19,14 +19,61 @@ $user->tag = array(
     ),
     4 => 'football',
 );
+$user->lang = array(
+    '.net', 'c#', 'php', 'css', 'html'
+);
 //print_r($user->load(new MongoId('50bec1d7fe608c679cb4dbbe')));
 //print_r($user->load(new MongoId('50bec14dfe608c679cb4dbbd')));
 print_r($user->find(array(
     'name' => 'dengjing',
 )));
+$user1 = new User();
+print_r($user1->find(array(), array('limit' => 1)));
 
 $demo = new Demo();
 //print_r($demo->load('50bd5f5cf3521a0f04000009'));
 //print_r($demo->load('50bd5f5cf3521a0f04000005'));
 print_r($demo->loadByIds(array('50bd5f5cf3521a0f04000009', '50bd5f5cf3521a0c04000000')));
+
+class Test implements ArrayAccess, Iterator, Countable {
+    function count() {
+        return 0;
+    }
+    
+    function offsetExists($offset) {
+        return $offset;
+    }
+    
+    function offsetGet($offset) {
+        return $offset;
+    }
+    
+    function offsetSet($offset, $value) {
+        return $value . $offset;
+    }
+    
+    function offsetUnset($offset) {
+        return $offset;
+    }
+    
+    function current() {
+        return null;
+    }
+    
+    function next() {
+        return null;
+    }
+    
+    function key() {
+        return null;
+    }
+    
+    function rewind() {
+        return null;
+    }
+    
+    function valid() {
+        return null;
+    }
+}
 ?>
