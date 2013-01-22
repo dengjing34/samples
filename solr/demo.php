0cffb577e73a6604b941fdb180efdc5201f94d6e
dengjing34 <dengjing34@vip.qq.com>
2013-01-22 18:56:40 +0800

    add sort and search result method

--- solr/demo.php
+++ solr/demo.php
@@ -4,8 +4,9 @@
  */
 require_once strtr(dirname(__FILE__) . DIRECTORY_SEPARATOR, '\\', '/') . "config/init.php";
 $searcher = new VideoSearcher();
-$searcher->timestampRangeQuery('addtime', '2012-12-12', '2013-01-16 23:55:55');
-$searcher->dayQuery('addtime', '2013-01-06');
-print_r($searcher->query);
+//$searcher->defaultQuery('-');
+echo "<pre>";
+$searcher->timestampQuery('addtime', '2012-12-25')->sort(array('addtime' => 'asc'));
+print_r($searcher->search(array('page' => 2)));
 
 ?>
--- solr/lib/Searcher.php
+++ solr/lib/Searcher.php
@@ -12,10 +12,9 @@ abstract class Searcher {
     const MAX_ROWS = 1000;
     const DEFAULT_WT = 'json';
     const DEFAULT_QUERY = '*:*';
-    const DEFAULT_ROWS = 10;
-    const DEFAULT_START = 0;
+    const DEFAULT_ROWS = 10;    
     private static $host = array(), $cores = array(), $counter = 0;
-    public $query = array();
+    private $q = self::DEFAULT_QUERY, $fq = array(), $sort = array();
     
     private function __construct() {
         ;
@@ -32,14 +31,29 @@ abstract class Searcher {
         return $this;
     }
     
+    /**
+     * 获取实例化的类名称
+     * @return string 实例化的类名称
+     */
     private function className() {
         return get_class($this);
     }
     
+    /**
+     * 获取设置的solr的core名称
+     * @return string solr core的名称
+     */
     protected function coreName() {
         return self::$cores[$this->className()];
     }
 
+    /**
+     * 
+     * @param string $url solr的请求地址
+     * @param boolean $waitSuccess 是否等待到成功 最多等待25秒
+     * @param array $postData 需要提交的数据
+     * @return boolean|json 失败返回false,成功返回请求的结果,默认是根据self::DEFAULT_WT来返回json数据
+     */
     private function request($url, $waitSuccess = false, array $postData = array()) {        
         $timeout = $waitSuccess ? 25 : 1;//if $waitSuccess timeout after 25s, otherwise timeout after 1s
         $ch = curl_init();
@@ -65,41 +79,76 @@ abstract class Searcher {
         return $body;
     }       
     
+    /**
+     * 增加请求solr的次数
+     * @return \Searcher
+     */
     private function increaseCounter() {
         self::$counter++;
         return $this;
     }
+
+    /**
+     * 搜索df(default search field)字段的关键词 为空的话则使用*:*
+     * @param string $string
+     * @return \Searcher
+     */
+    public function defaultQuery($string = self::DEFAULT_QUERY) {
+        $string = strlen(trim($string)) == 0 || $string == self::DEFAULT_QUERY ? self::DEFAULT_QUERY : $this->solrEscape($string);
+        $this->q = $string;
+        return $this;
+    }
     
+    /**
+     * 添加fq(filter query)的查询
+     * @param string $field
+     * @param string $value
+     * @return \Searcher
+     */
     public function query($field, $value) {
-        $this->query[] = "{$field}:{$value}";
+        if (strlen(trim($field)) !=0 && strlen(trim($value)) != 0) {            
+            $this->fq[] = "{$field}:{$value}";
+        }        
         return $this;
     }
     
+    /**
+     * 对某个字段进行范围搜索
+     * @param string $field 搜索的字段名
+     * @param string $lower 搜索范围开始值
+     * @param string $upper 搜索范围结束值
+     * @return \Searcher
+     */
     public function rangeQuery($field, $lower = '*', $upper = '*') {
-        $this->query($field, "[{$lower} TO {$upper}]");
+        if (strlen(trim($field)) != 0) {
+            $this->query($field, "[{$lower} TO {$upper}]");
+        }        
         return $this;
     }
     
+    /**
+     * 搜索某一整天时间段的结果 查询字段需要是索引的date或tdate类型
+     * @param string $field 查询字段 其在solr中的fieldType需要是date或tdate类型 如2000-01-01T01:01:01Z
+    * @param string $value 日期如yyyy-mm-dd的格式,会自动补上当天开始和结束时分秒的值
+     * @return \Searcher
+     */
     public function dateQuery($field, $value) {
-        if (preg_match('/^\d{4}-\d{2}-\d{2}$/', $value)) {
+        if (preg_match('/^\d{4}-([0][1-9]|1[012])-([012][0-9]|3[01])$/', $value)) {
             $this->query($field, "[{$value}T00:00:00Z TO {$value}T23:59:59Z]");
         }
         return $this;
     }
     
-    public function dateRangeQuery($field, $startDate = '*', $endDate = '*') {
-        $pattern = '/^(\d{4}-\d{2}-\d{2}|\*)$/';
-        if (preg_match($pattern, $startDate) && preg_match($pattern, $endDate)) {
-            $startDateTime = $startDate != '*' ? "{$startDate}T00:00:00Z" : $startDate;
-            $endDateTime = $endDate != '*' ? "{$endDate}T23:59:59Z" : $endDate;
-            $this->rangeQuery($field, $startDateTime, $endDateTime);
-        }
-        return $this;
-    }
-    
-    public function dateTimeRangeQuery($field, $startDateTime = '*', $endDateTime = '*') {
-        $pattern = '/^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}|\d{4}-\d{2}-\d{2}|\*)$/';        
-        if (preg_match($pattern, $startDateTime) && preg_match($pattern, $endDateTime)) {
+    /**
+     * 搜索某一段时间范围的结果 查询字段需要是索引的date或tdate类型
+     * @param string $field 查询字段 其在solr中的fieldType需要是date或tdate类型 如2000-01-01T01:01:01Z
+     * @param type $startDateTime 时间范围开始值,支持yyyy-mm-dd | yyyy-mm-dd hh:mm | yyyy-mm-dd hh:mm:ss 的格式 用*为不限
+     * @param type $endDateTime 时间范围结束值,支持yyyy-mm-dd | yyyy-mm-dd hh:mm | yyyy-mm-dd hh:mm:ss 的格式 用*为不限
+     * @return \Searcher
+     */
+    public function dateRangeQuery($field, $startDateTime = '*', $endDateTime = '*') {
+        $pattern = '/^\d{4}-([0][1-9]|1[012])-([012][0-9]|3[01])$|^\d{4}-([0][1-9]|1[012])-([012][0-9]|3[01])\s([01][0-9]|2[0123]):[0-5][0-9](:[0-5][0-9])?$|^\*$/';        
+        if (preg_match($pattern, $startDateTime, $m) && preg_match($pattern, $endDateTime)) {            
             $startDateTime = $startDateTime != '*' ? $this->formatDateTime($startDateTime) : $startDateTime;
             $endDateTime = $endDateTime != '*' ? $this->formatDateTime($endDateTime) : $endDateTime;
             $this->rangeQuery($field, $startDateTime, $endDateTime);            
@@ -107,9 +156,15 @@ abstract class Searcher {
         return $this;
     }
     
+    /**
+     * 搜索某一段时间范围的结果 查询字段需要是索引的timestamp
+     * @param type $field 查询字段 其在solr中的fieldType需要是int或tint等数字类型 如1325350861
+     * @param type $startDateTime 时间范围开始值,支持yyyy-mm-dd | yyyy-mm-dd hh:mm | yyyy-mm-dd hh:mm:ss 的格式 用*为不限
+     * @param type $endDateTime 时间范围结束值,支持yyyy-mm-dd | yyyy-mm-dd hh:mm | yyyy-mm-dd hh:mm:ss 的格式 用*为不限
+     * @return \Searcher
+     */
     public function timestampRangeQuery($field, $startDateTime = '*', $endDateTime = '*') {
-        $pattern = '/^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}|\d{4}-\d{2}-\d{2}|\*)$/';
-        var_dump(preg_match($pattern, $startDateTime));
+        $pattern = '/^\d{4}-([0][1-9]|1[012])-([012][0-9]|3[01])$|^\d{4}-([0][1-9]|1[012])-([012][0-9]|3[01])\s([01][0-9]|2[0123]):[0-5][0-9](:[0-5][0-9])?$|^\*$/';
         if (preg_match($pattern, $startDateTime) && preg_match($pattern, $endDateTime)) {
             $startDateTime = $startDateTime != '*' ? strtotime($startDateTime) : $startDateTime;
             $endDateTime = $endDateTime != '*' ? strtotime($endDateTime) : $endDateTime;
@@ -117,7 +172,26 @@ abstract class Searcher {
         }
         return $this;
     }
+
+    /**
+     * 搜索某一整天时间段的结果 查询字段需要是索引的int或tint类型
+     * @param string $field 查询字段 其在solr中的fieldType需要是int或tint类型 如1325350861
+    * @param string $value 日期如yyyy-mm-dd的格式,会自动补上当天开始和结束时分秒的值
+     * @return \Searcher
+     */    
+    public function timestampQuery($field, $value) {
+        if (preg_match('/^\d{4}-([0][1-9]|1[012])-([012][0-9]|3[01])$/', $value)) {
+            $this->query($field, '[' . strtotime("{$value} 00:00:00") . ' TO ' . strtotime("{$value} 23:59:59") . ']');
+        }
+        return $this;        
+    }
     
+    /**
+     * 搜索查询字段包括一系列值
+     * @param string 查询的字段
+     * @param array $values 查询字段包括的一系列值
+     * @return \Searcher
+     */
     public function inQuery($field, array $values = array()) {
         foreach ($values as $eachValue) {
             $this->query($field, $eachValue);
@@ -125,15 +199,35 @@ abstract class Searcher {
         return $this;
     }
     
-    public function parseQuery() {
-        return $this->query;
+    /**
+     * 生成fq[filter query]的查询字符
+     * @return string
+     */
+    private function parseFq() {        
+        return implode(' ', $this->fq);
     }
     
+    
+    /**
+     * 格式化时间为solr的date类型时间
+     * @param string|int $date 可以是时间戳或YYYY-MM-DD | YYYY-MM-DD HH:MM | YYYY-MM-DD HH:MM:SS
+     * @return string solr的date类型的查询格式 如 2012-12-13T12:13:14Z
+     */
     public function formatDateTime($date) {
         if (ctype_digit((string)$date)) {
             return date('Y-m-d\TH:i:s\Z', $date);
         }
         return date('Y-m-d\TH:i:s\Z', strtotime($date));
+    }
+    
+    /**
+     * 过滤q(default query)的查询字符
+     * @param type $value 查询的字符
+     * @return string 过滤后的查询字符
+     */
+    public function solrEscape($value) {        
+        $pattern = '/(\+|-|&&|\|\||!|\s|\(|\)|\{|}|\[|]|\^|"|~|\*|\?|:|\\\)/';
+        return preg_replace($pattern, '\\\$1', $value);        
     }
 
 
@@ -141,28 +235,105 @@ abstract class Searcher {
         
     }
     
+    /**
+     * 根据q[default query], fq[filter query], sort 来搜索得出结果
+     * @param array $options 搜索选项可有的参数
+     * <pre>
+     * <code>
+     * $options = array(
+     *     'waitSuccess' => false,//是否等待成功 最多等待25秒
+     *     'rows' => 10,//返回行数
+     *     'page' => 1,//页数
+     * );
+     * <code>
+     * <pre>
+     * @return array 查询结果 
+     * <pre>
+     * <code>
+     * $result = array(
+     *     'currentPage' => 1,//当前页
+     *     'totalPage' => 224,//总页数
+     *     'pageSize' => 10,//每页记录数
+     *     'numFound' => 2233,//总记录数
+     *     'start' => 0,//跳过条数
+     *     'docs' => array(
+     *         0 => array(
+     *             'id' => 1,
+     *             ...
+     *         ),
+     *         1 => array(
+     *             'id' => 2,
+     *             ...
+     *         ),
+     *         ...
+     *     ),
+     * );
+     * </code>
+     * </pre>
+     */
     public function search(array $options = array()) {
-        $q = self::DEFAULT_QUERY;
+        $result = false;
+        $q = $this->q;
+        $options['fq'] = $this->parseFq();
+        $options['sort'] = $this->parseSort();
+        $options['rows'] = isset($options['rows']) ? $this->validRows($options['rows']) : self::DEFAULT_ROWS;
+        $options['page'] = isset($options['page']) && ctype_digit((string)$options['page']) && $options['page'] > 0 ? $options['page'] : 1;
+        $options['start'] = ($options['page'] - 1) * $options['rows'];
         $waitSuccess = isset($options['waitSuccess']) ? true : false;
-        $data = $this->request($this->buildUrl($q, $options), $waitSuccess);
-        return json_decode($data);  
+        if ($data = $this->request($this->buildUrl($q, $options), $waitSuccess)) {
+            $data = json_decode($data, true);
+            $result['currentPage'] = $options['page'];
+            $result['totalPage'] = ceil($data['response']['numFound'] / $options['rows']);
+            $result['pageSize'] = $options['rows'];            
+            $result += $data['response'];
+        }
+        return $result;
     }        
     
-    private function buildUrl($q = '', $options = array()) {
+    /**
+     * 构建请求的url地址
+     * @param string $q
+     * @param array $options
+     * @return type
+     */
+    private function buildUrl($q = '', array $options = array()) {
         $params = array(
-            'omitHeader' => 'true',
-            'q' => strlen($q) == 0 ? self::DEFAULT_QUERY : $q,
+            'omitHeader' => 'true',//忽略请求状态和时间
+            'q' => strlen(trim($q)) == 0 ? self::DEFAULT_QUERY : $q,            
             'wt' => self::DEFAULT_WT,            
-            'rows' => isset($options['rows']) && ctype_digit((string)$options['rows']) ? $this->validRows($options['rows']) : self::DEFAULT_ROWS,
-            'start' => isset($options['start']) && ctype_digit((string)$options['start']) ? $options['start'] : self::DEFAULT_START,
+            'rows' => $options['rows'],
+            'start' => $options['start'],
         );
-        if (isset($options['sort'])) $params['sort'] = $options['sort']; 
+        if (isset($options['sort'])) $params['sort'] = $options['sort'];
+        if (!empty($options['fq'])) $params['fq'] = $options['fq'];        
         return "{$this->slaveCore()}?" . http_build_query($params);
     }
 
 
+    /**
+     * 限制solr一次请求最返回的行数
+     * @param int $rows 实际请求的返回行数
+     * @return int 允许返回的行数
+     */
     private function validRows($rows) {
-        return $rows <= self::MAX_ROWS ? $rows : self::MAX_ROWS;
+        return ctype_digit((string)$rows) && $rows > 0 && $rows <= self::MAX_ROWS ? $rows : self::MAX_ROWS;
+    }
+   
+    public function sort(array $sorts = array()) {
+        $allowed = array('asc', 'desc');
+        foreach ($sorts as $field => $sort) {
+            $sort = strtolower($sort);
+            if (strlen($field) != 0 && in_array($sort, $allowed)) {
+                $this->sort[$field] = $sort;
+            }                
+        }
+        return $this;
+    }
+    
+    public function parseSort() {
+        return implode(' ', array_map(function($field, $sort) {
+            return "{$field} {$sort}";
+        }, array_keys($this->sort), $this->sort));                    
     }
     
     private function host() {
