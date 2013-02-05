7ef64d3f667a62cefa6f30f4deca161b3e1697c5
dengjing34 <dengjing34@vip.qq.com>
2013-02-05 16:14:48 +0800

    add delete by query demo

--- solr/delete.php
+++ solr/delete.php
@@ -6,6 +6,7 @@ header('Content-type:text/html; charset=utf-8');
 require_once strtr(dirname(__FILE__) . DIRECTORY_SEPARATOR, '\\', '/') . "config/init.php";
 $id = isset($_POST['id']) ? $_POST['id'] : null;
 $ids = isset($_POST['ids']) ? $_POST['ids'] : null;
+$query = isset($_POST['query']) ? $_POST['query'] : null;
 if ($_POST) {
     $searcher = new VideoSearcher();
     $deleted = array();
@@ -25,7 +26,12 @@ if ($_POST) {
             die;            
         }
         $deleted += $idArr;
-    }    
+    }
+    if ($query && !$searcher->deleteByQuery($query)) {
+        echo '<pre>';
+        print_r($searcher->lastError());
+        die;
+    }
     echo implode(',', $deleted) . "had been deleted <a href=\"{$_SERVER['HTTP_REFERER']}\">back</a>";
     die;
 } 
@@ -33,6 +39,7 @@ $html = <<<EOT
 <form method="post">
     id:<input type="text" name="id" /><br />
     ids:<input type="text" name="ids" /><br />
+    query:<input type="text" name="query" /><br />
     <button type="submit">删除</button>
 </form>
 EOT;
